e884c7bc8dbb6c8fea3692c7127bb7ee7975aa56
Fredrik Eklund <fredrik@bayville.se>
2024-01-12 19:50:31 +0100

    Fixed links, scripts, fav-icon added webshop.js

--- js/main.js
+++ js/main.js
@@ -20,17 +20,18 @@ function toggleMenu() {
     }
 }
 
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'center',
-  contain: true,
-  wrapAround: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+if (document.querySelector('.main-carousel')){
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true
+  });
+  
+  // element argument can be a selector string
+  //   for an individual element
+  var flkty = new Flickity( '.main-carousel', {
+    // options
+  });
+}
